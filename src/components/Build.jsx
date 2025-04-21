7e1620785fc9e046620dcd6f587e2720dbf5fbc8
Harsh Jat <harshjat434@gmail.com>
2025-04-21 12:42:10 +0530

    Upadate Buiild, Connect

--- src/components/Build.jsx
+++ src/components/Build.jsx
@@ -1,10 +1,28 @@
 // Example: Build.jsx
+// import React from "react";
+
+// function Build() {
+//   return (
+//     <section >
+//       <h1>Build Section</h1>
+//     </section>
+//   );
+// }
+
+// export default Build;
+
+// BuildReal.js
 import React from "react";
+import "./build.css";
+<style>
+{/* @import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap'); */}
+@import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&family=Outfit:wght@100..900&display=swap');
+</style>
 
 function Build() {
   return (
-    <section >
-      <h1>Build Section</h1>
+    <section className="build-real-section">
+      <h2 className="build-real-text">Let’s build<br />something<br /><span>REAL.</span></h2>
     </section>
   );
 }
--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -1,10 +1,35 @@
 // Example: Connect.jsx
+// import React from "react";
+
+// function Connect() {
+//   return (
+//     <section >
+//       <h1>Connect Section</h1>
+//     </section>
+//   );
+// }
+
+// export default Connect;
+
+// Connect.js
 import React from "react";
+import "./connect.css";
+<style>
+{/* @import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap'); */}
+@import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&family=Outfit:wght@100..900&display=swap');
+</style>
 
 function Connect() {
   return (
-    <section >
-      <h1>Connect Section</h1>
+    <section className="connect-section">
+      <div className="connect-text">
+        <p>Connect us.</p>
+        <div className="connect-roles">
+          <span>As client.</span>
+          <span>As employee.</span>
+        </div>
+        <button className="connect-button">Click Here!</button>
+      </div>
     </section>
   );
 }
